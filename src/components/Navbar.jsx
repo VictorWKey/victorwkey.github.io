018468250fc60a6c89ab693a2cfaf8c99b164fcc
victorwkey <victorwkey@gmail.com>
2024-03-20 22:23:32 -0600

    Portfolio v1.2.0 Fix error and change navbar color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,14 +7,14 @@ function Navbar() {
 
 
   return (
-    <nav className="z-20 bg-white flex justify-left sm:justify-center items-center gap-12 py-2 px-4 border-4 border-b-primary fixed top-0 w-full">
-      <a href="#top" className="hidden sm:font-semibold sm:text-gray-800 sm:flex sm:cursor-pointer hover:text-primary ">Inicio</a>
-      <a href="#projects" className="hidden sm:font-semibold sm:text-gray-800 sm:flex sm:cursor-pointer hover:text-primary">Proyectos</a>
-      <a href="#skills" className="hidden sm:font-semibold sm:text-gray-800 sm:flex sm:cursor-pointer hover:text-primary">Habilidades</a>
+    <nav className="z-20 bg-primary  flex justify-left sm:justify-center items-center gap-12 py-3 px-4 fixed top-0 w-full ">
+      <a href="#top" className="hidden sm:font-semibold sm:text-white sm:flex sm:cursor-pointer hover:text-gray-100">Inicio</a>
+      <a href="#projects" className="hidden sm:font-semibold sm:text-white sm:flex sm:cursor-pointer hover:text-gray-100">Proyectos</a>
+      <a href="#skills" className="hidden sm:font-semibold sm:text-white sm:flex sm:cursor-pointer hover:text-gray-100">Habilidades</a>
 
-      <a href="#about-me"className="hidden sm:font-semibold sm:text-gray-800 sm:flex sm:cursor-pointer hover:text-primary ">Sobre mi</a>
+      <a href="#about-me"className="hidden sm:font-semibold sm:text-white sm:flex sm:cursor-pointer hover:text-gray-100">Sobre mi</a>
       <div onClick={() => isMovilMenuOpen.set(true)}>
-        <img className="flex sm:hidden" src="../../utility-icons/menu_icon.svg"/>
+        <img className="flex sm:hidden size-8" src="../../public/icons/Menu.svg"/>
       </div>
     </nav>
   )
